a86cdb469a121becd59795ca12b17a65d6354e31
highjun <highjun10170@gmail.com>
2021-04-28 17:42:01 +0900

    10일차

--- 2156.cpp
+++ 2156.cpp
@@ -2,32 +2,49 @@
 #include<memory.h>
 using namespace std;
 
-int cache[10000][2];
+// int cache[10000][2];
 
+// void calMaxScore(int score[], int n){
+//     if(n==0 && cache[0][0] == -1){
+//         cache[0][0] = score[0];
+//         cache[0][1] = 0;
+//     }else if(n==1 && cache[1][0]== -1){
+//         cache[1][0] = score[1];
+//         cache[1][1] = score[1]+ score[0];
+//     }else if(n==2 && cache[2][0]== -1){
+//         cache[2][0] = score[2] + score[0];
+//         cache[2][1] = score[2] + score[1];
+//     }else if(cache[n][0] == -1){
+//         calMaxScore(score, n-1);
+//         calMaxScore(score, n-2);
+//         calMaxScore(score, n-3);
+//         int tmp = max(cache[n-3][0], cache[n-3][1]);
+//         tmp = max(tmp,cache[n-2][0]);
+//         tmp = max(tmp,cache[n-2][1]);
+//         cache[n][0] = tmp + score[n];
+//         cache[n][1] = cache[n-1][0] + score[n];
+//     }
+// }
+
+int cache[10000];
 void calMaxScore(int score[], int n){
-    if(n==0 && cache[0][0] == -1){
-        cache[0][0] = score[0];
-        cache[0][1] = 0;
-    }else if(n==1 && cache[1][0]== -1){
-        cache[1][0] = score[1];
-        cache[1][1] = score[1]+ score[0];
-    }else if(n==2 && cache[2][0]== -1){
-        cache[2][0] = score[2] + score[0];
-        cache[2][1] = score[2] + score[1];
-    }else if(cache[n][0] == -1){
+    if(n == 0 && cache[n] == -1){
+        cache[0] = score[0];
+    }else if(n==1 && cache[n] == -1){
+        cache[1] = score[0]+ score[1];
+    }else if(n==2 && cache[n] == -1){
+        cache[2] = max(max(score[0]+ score[2], score[0] + score[1]), score[1]+ score[2]);
+    }else if(cache[n]== -1){
         calMaxScore(score, n-1);
         calMaxScore(score, n-2);
         calMaxScore(score, n-3);
-        int tmp = max(cache[n-3][0], cache[n-3][1]);
-        tmp = max(tmp,cache[n-2][0]);
-        tmp = max(tmp,cache[n-2][1]);
-        cache[n][0] = tmp + score[n];
-        cache[n][1] = cache[n-1][0] + score[n];
+        cache[n] = max(cache[n-1], score[n] +cache[n-2]);
+        cache[n] = max(cache[n], score[n]+score[n-1]+cache[n-3]);
     }
 }
 
 int main(){
-    memset(cache, -1, 10000*2*sizeof(int));
+    memset(cache, -1, 10000*sizeof(int));
     int N;
     cin >> N;
     int score[N];
@@ -36,13 +53,8 @@ int main(){
     }
     calMaxScore(score, N-1);
     // for( int i = 0; i< N; i++){
-    //     cout << score[i] << " " << cache[i][0] <<" " << cache[i][1] << endl;
+    //     cout << score[i] << " " << cache[i] << endl;
     // }
-    int result = 0;
-    for(int i =0;i<N;i++){
-        result = max(result, cache[i][0]);
-        result = max(result, cache[i][1]);
-    }
-    cout << result << endl;
+    cout << cache[N-1] << endl;
     return 0;
 }
